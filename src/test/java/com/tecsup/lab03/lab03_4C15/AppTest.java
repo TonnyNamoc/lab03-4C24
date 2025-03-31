c9bf723a0f8a9a1b7c21cc38d5e12d15d4fd137c
Tonny Alexander Namoc Julca <tonny.namoc@tecsup.edu.pe>
2025-03-31 14:11:28 -0500

    First Commit

---
+++ src/test/java/com/tecsup/lab03/lab03_4C15/AppTest.java
@@ -0,0 +1,19 @@
+package com.tecsup.lab03.lab03_4C15;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
